ceeef6d1a77caf95bb1d6e23f749e861c4a05630
veenrokroot <veenrokdalv@gmail.com>
2022-10-23 20:07:46 +0300

    delete db sqlite3

--- config/settings.py
+++ config/settings.py
@@ -69,10 +69,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3'
-    },
-    '_default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': env.str('POSTGRES_DB'),
         'USER': env.str('POSTGRES_USER'),
